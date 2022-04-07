c1b25f22dd5af614a80d7695e4a53a302ec80335
unniznd <au6695@gmail.com>
2022-04-07 09:18:44 +0530

    production settings updated

--- takehome/prod_settings.py
+++ takehome/prod_settings.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 
-
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = "os.environ.get('SECRET_KEY')"
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unniku$default',
+        'USER': 'unniku',
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': 'unniku.mysql.pythonanywhere-services.com>',
     }
 }
 
-ALLOWED_HOSTS = ['localhost']
+
+ALLOWED_HOSTS = ['unniku.pythonanywhere']
 MEDIA_ROOT = BASE_DIR/'media/'
