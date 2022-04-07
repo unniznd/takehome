1025f80b9dd3a59553b0508fb603b3b5e2fe5ab5
unniznd <au6695@gmail.com>
2022-04-07 10:13:36 +0530

    production settings minor changes

--- takehome/prod_settings.py
+++ takehome/prod_settings.py
@@ -1,20 +1,33 @@
 from pathlib import Path
-
 import os
+import json
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+with open('secret_key.json') as f:
+    secret = json.load(f)
+
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = ['takehomeimager.herokuapp.com']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'unniku$default',
-        'USER': 'unniku',
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': 'unniku.mysql.pythonanywhere-services.com>',
+    "default": {
+        "ENGINE": secret['ENGINE'],
+        "NAME": secret['NAME'],
+        "USER": secret['USER'],
+        "PASSWORD":os.environ.get('PASSWORD'),
+        "HOST":secret['HOST'],
+        "PORT": secret['PORT']
     }
 }
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': "dhw9oa5fm",
+    'API_KEY': os.environ.get("API_KEY"),
+    'API_SECRET': os.environ.get("API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-ALLOWED_HOSTS = ['unniku.pythonanywhere']
-MEDIA_ROOT = BASE_DIR/'media/'
